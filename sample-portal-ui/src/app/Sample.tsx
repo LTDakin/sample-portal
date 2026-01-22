cde62696c0471f98af4624fc2ebf10a6228d62fb
Lloyd Dakin <taidakin99lol@gmail.com>
2026-01-21 18:54:49 -0800

    add calender date and notes to display

--- sample-portal-ui/src/app/Sample.tsx
+++ sample-portal-ui/src/app/Sample.tsx
@@ -1,15 +1,21 @@
 interface SampleProps {
   name: string;
   dateOfBirth: string;
+  collectionDate?: string;
+  notes?: string;
   onEnterData: () => void;
 }
 
 export default function Sample(props: SampleProps) {
   return (
     <>
-      <div className="flex flex-col gap-4 mt-10">
-        <h1 className="font-semibold">Name: {props.name}</h1>
-        <h1 className="font-semibold">Date of Birth: {props.dateOfBirth}</h1>
+      <div className="flex flex-col gap-4  h-10">
+        <h1 className="font-bold">Name: {props.name}</h1>
+        <h1 className="font-bold">Date of Birth: {props.dateOfBirth}</h1>
+        {props.collectionDate && (
+          <h1 className="font-bold">Collection Date: {props.collectionDate}</h1>
+        )}
+        {props.notes && <h1 className="font-bold">Notes: {props.notes}</h1>}
       </div>
       <button
         className="bg-black text-white px-4 py-2 rounded hover:bg-gray-800 transition-colors mt-15"
